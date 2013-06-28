f627671f10639e4f30d7f28d477794c3f17c3673
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2013-06-28 19:08:11 +0400

    Contact definitions

---
+++ sinapse-grid-core/src/ru/primetalk/sinapse/AutomataBuilder.scala
@@ -0,0 +1,95 @@
+///////////////////////////////////////////////////////////////
+// © ООО «Праймтолк», 2011-2013                              //
+// Все права принадлежат компании ООО «Праймтолк».           //
+///////////////////////////////////////////////////////////////
+/**
+ * ${PROJECT_NAME}
+ * © Primetalk Ltd., 2013.
+ * All rights reserved.
+ * Authors: A.Zhizhelev, A.Nehaev, P. Popov
+ * (2-clause BSD license) See LICENSE
+ *
+ * Created: 28.06.13, zhizhelev
+ */
+package ru.primetalk.sinapse
+
+/** The builder creates a state machine with `State` type of state.*/
+trait AutomataBuilder[State] extends SystemBuilder {
+	def initialState : State
+	protected def stateName = "automatonState"
+	protected val automatonState = state[State](stateName, initialState)
+
+	private val zippedCache = scala.collection.mutable.Map[Contact[_], Contact[_]]()
+	def zipped[T](c : Contact[T]) : Contact[(State, T)] = {
+		zippedCache.getOrElseUpdate(c, c zipWithState (automatonState, "(State,_)")).asInstanceOf[Contact[(State, T)]]
+	}
+	private val zippedFilteredCache = scala.collection.mutable.Map[(Contact[_], State), Contact[_]]()
+	def zippedFiltered[T](c : Contact[T], s:State) : Contact[T] = {
+		zippedFilteredCache.getOrElseUpdate((c,s), zipped(c) flatMap (p ⇒ {
+				if (p._1 == s)
+					Seq(p._2)
+				else
+					Seq()
+			},
+				""+s+"?")).asInstanceOf[Contact[T]]
+	}
+	val saveToState = contact[State]("saveToState")
+	lazy val onTransition = {
+		val c = contact[(State, State)]("onTransition")
+		saveToState -> c zipWithState automatonState
+		c
+	}
+	saveToState.updateState(automatonState, "State!")((old, s) ⇒ s)
+	implicit class StateContact[T](c : Contact[T]) {
+		/** Constructs another contact that will get data when the automata is in the desired state. */
+		def when(s : State) : Contact[T] =
+			zippedFiltered(c,s)
+		/**
+		  * When signal appears on the contact the Automaton
+		  *  moves to the desired state.
+		  */
+		def toState(s : State, name : String = ""):Contact[T]= {
+//			c.updateState(automatonState, nextLabel(name, ""+s+"!"))((s1,t) => s) //.updateState(automatonState, ""+s+"!")((old, t)=>s)
+			c -> saveToState map (t ⇒ s, nextLabel(name, ""+s+"!"))
+			c
+		}
+		/** Switches to state and do some work along the way */
+		def moveToState(s : State, name : String = "")(fun : T ⇒ Any) {
+			c -> saveToState map (t ⇒ { fun(t); s }, nextLabel(name, ""+s+"!"))
+		}
+		def update(fun:(State,T)=>State, name : String = "") = {
+			c.updateState(automatonState, nextLabel(name, "update automaton"))(fun)
+			c
+//			zipped(c) -> saveToState map (p=>fun(p._1, p._2), nextLabel(name, "update"))
+		}
+		/** Updates state unconditionally (ignores input data).*/
+		def updateUnconditionally(fun:State=>State, name : String = ""){
+			c.updateState(automatonState, nextLabel(name, "update uncoditionally"))((s, t) =>fun(s))
+//			zipped(c) -> saveToState map (p=>fun(p._1), nextLabel(name, "update uncoditionally"))
+		}
+
+	}
+
+	//Methods that deal with VAR currentConstructingState
+	private var currentConstructingState :State = initialState
+	def startBuildingState(s:State) = {
+		currentConstructingState = s
+		s
+	}
+	def inState(s : State)(body : ⇒ Unit) {
+		currentConstructingState = s
+		try {
+			body
+		} finally {
+			currentConstructingState = initialState
+		}
+	}
+//	implicit class StateConstructor(s : State) {
+		def on[T](c:Contact[T]): Contact[T] = {new StateContact(c).when(currentConstructingState)}
+//	}
+
+//	def wait[T](c:Contact[T])(implicit s : State): Function1[()=>State, Unit] = (f:()=>State)=>{
+//		(new StateContact(c).when(s) -> saveToState).map(m => f())//.toState(s)
+//	}
+//	}
+}
---
+++ sinapse-grid-core/src/ru/primetalk/sinapse/BasicSystemBuilder.scala
@@ -0,0 +1,142 @@
+///////////////////////////////////////////////////////////////
+// © ООО «Праймтолк», 2011-2013                              //
+// Все права принадлежат компании ООО «Праймтолк».           //
+///////////////////////////////////////////////////////////////
+/**
+ * SinapseGrid
+ * © Primetalk Ltd., 2013.
+ * All rights reserved.
+ * Authors: A.Zhizhelev, A.Nehaev, P. Popov
+ * (2-clause BSD license) See LICENSE
+ *
+ * Created: 28.06.13, zhizhelev
+ */
+package ru.primetalk.sinapse
+
+import scala.annotation.tailrec
+
+/**
+ * This builder supports step-by-step creation of contact system. At the end
+ * one must convert it to [[ru.primetalk.sinapse.StaticSystem]].
+ */
+trait BasicSystemBuilder {
+  private var name = getClass.getSimpleName.
+    replaceAllLiterally("Builder", "").
+    replaceAllLiterally("BuilderC", "").
+    replaceAllLiterally("$", "_")
+
+  def setSystemName(name : String) {
+    this.name = name
+  }
+
+  import scala.collection.mutable
+
+  protected val contacts = mutable.ListBuffer[Contact[_]]()
+  protected val privateStateHandles = mutable.ListBuffer[StateHandle[_]]()
+  protected val links = mutable.ListBuffer[Link[_, _, Nothing, Any]]()
+  protected val components = mutable.ListBuffer[OuterSystem]()
+  protected val inputContacts = mutable.Set[Contact[_]]()
+  protected val outputContacts = mutable.Set[Contact[_]]()
+
+  /** Constructs new version of static system. */
+  def toStaticSystem = StaticSystem(
+    /** A subset of contacts */
+    inputContacts.toList.distinct,
+    outputContacts.toList.distinct,
+    privateStateHandles.toList,
+    components.toList reverse_::: links.toList,
+    name: String)
+
+  /**
+   * Sets this builder to the read only mode. Subsequent modifications will lead to
+   * exceptions.
+   */
+  def readOnly() {
+    isReadOnly = true
+  }
+
+  private var isReadOnly = false
+
+  protected def assertWritable() {
+    if (isReadOnly)
+      throw new IllegalStateException(s"The system builder '$name' is in read only mode.")
+  }
+
+  /**
+   * Makes the builder unmodifiable and returns completed static system.
+   */
+  def complete() = {
+    readOnly()
+    toStaticSystem
+  }
+
+
+  /**
+   * Create StateHandle and add it to the system
+   */
+  def state[S](name : String, initialValue : S) : StateHandle[S] = {
+    assertWritable()
+    addStateHandle( StateHandle[S](name, initialValue))
+  }
+  /**
+   * Add StateHandle to the system
+   */
+  def addStateHandle[S](sh : StateHandle[S]) : StateHandle[S] = {
+    assertWritable()
+    if(!privateStateHandles.contains(sh))
+      privateStateHandles += sh
+    sh
+  }
+  def addLink[T1, T2](from:Contact[T1], to:Contact[T2], info: LinkInfo[T1, T2]):Contact[T2] = {
+    val link = Link(from, to, info)
+    addLink(link)
+  }
+  /**
+    */
+  def addLink[T1, T2](link : Link[T1, T2, T1, T2]):Contact[T2] = {
+    assertWritable()
+    if(outputContacts.contains(link.from))
+      throw new IllegalArgumentException(s"The link $link cannot be added because ${link.from.name} is output contact.")
+    links += link
+    link.to
+  }
+  def addComponent(component:OuterSystem){
+    components += component
+  }
+  /**
+   * Subsystem.
+   * It can have a few input contacts (any number), however,
+   * it will process signals by one.
+   * If the subsystem has output contacts (it usually has), then the result of subsystem
+   * processing will appear on the same contacts of the parent system.
+   */
+  def addSubsystem(s:StaticSystem, sharedStateHandles: StateHandle[_]*)= {
+    sharedStateHandles.foreach(addStateHandle(_))
+    val s0withoutShared = s.s0 -- sharedStateHandles
+    components += new InnerSystem(s, state(s.name+"State", s0withoutShared), sharedStateHandles.toList)
+  }
+
+  // Static analysis of the system's graph
+
+  /** returns one step successors from the given contact */
+  def successors(c:Contact[_]):List[Contact[_]] = {
+    val linkSucceccors = links.toList.filter(_.from == c).map(_.to)
+    val compSucceccors = components.toList.filter(_.inputContacts.contains(c)).flatMap(_.outputContacts)
+    (linkSucceccors ++ compSucceccors).distinct
+  }
+  /** Calculates the number of transitions from c1 to that contact. If the contact is not reachable
+    *  then the distance is equals = -1*/
+  def minDistance(c1 : Contact[_], c2 : Contact[_]) = {
+    @tailrec
+    def minDistance(toCheck:Set[Contact[_]], exclude:Set[Contact[_]], dist:Int = 0):Int = {
+      if(toCheck.isEmpty)
+        -1
+      else if(toCheck.contains(c2))
+        dist
+      else
+        minDistance((toCheck flatMap successors) -- exclude, exclude ++ toCheck, dist + 1)
+    }
+    minDistance(Set(c1), Set())
+  }
+
+}
