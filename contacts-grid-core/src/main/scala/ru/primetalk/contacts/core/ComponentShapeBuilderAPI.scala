3ef4e6680839bc4fabc457629eeeccf7defbe071
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2019-09-25 23:12:29 +0300

    Add basic component operations

--- contacts-grid-core/src/main/scala/ru/primetalk/contacts/core/ComponentShapeBuilderAPI.scala
+++ contacts-grid-core/src/main/scala/ru/primetalk/contacts/core/ComponentShapeBuilderAPI.scala
@@ -1,30 +1,35 @@
 package ru.primetalk.contacts.core
 
 trait ComponentShapeBuilderAPI extends TypeSets {
-  type Contact[T]
+
+  case class ComponentShape2[InputShape<:TypeSet, OutputShape <: TypeSet](inputs: InputShape, outputs: OutputShape)
+
+  type Contact <: {
+    type T
+  }
   sealed trait ComponentShape {
     type InputShape <: TypeSet
     type OutputShape <: TypeSet
-    def inputs: InputShape
-    def outputs: OutputShape
+    val inputs: InputShape
+    val outputs: OutputShape
   }
 
   object EmptyComponentShape extends ComponentShape {
     override type InputShape = ∅
     override type OutputShape = ∅
 
-    override def inputs: ∅ = ∅
+    override val inputs: ∅ = ∅
 
-    override def outputs: ∅ = ∅
+    override val outputs: ∅ = ∅
   }
 
-  def addInput[T, C<:Contact[T],
+  def addInput[C<:Contact,
 //    InputShape0 <: TypeSet,
 //    OutputShape0 <: TypeSet,
     Comp0 <: ComponentShape](c: C, componentShape: Comp0)(
       implicit
-        inputsAreContacts: EachElementIsSubtype[Contact[T], Comp0#InputShape],
-        outputsAreContacts: EachElementIsSubtype[Contact[T], Comp0#OutputShape],
+        inputsAreContacts: EachElementIsSubtype[Contact, Comp0#InputShape],
+        outputsAreContacts: EachElementIsSubtype[Contact, Comp0#OutputShape],
       InputShapeAddC: AddWrapper[C, Comp0#InputShape]
     ): ComponentShape{
       type InputShape = InputShapeAddC.AuxPlus
@@ -33,8 +38,61 @@ trait ComponentShapeBuilderAPI extends TypeSets {
       override type InputShape = InputShapeAddC.AuxPlus
       override type OutputShape = Comp0#OutputShape
 
-      override def inputs: InputShapeAddC.AuxPlus = InputShapeAddC.auxPlus(c, componentShape.inputs)
+      override val inputs: InputShapeAddC.AuxPlus = InputShapeAddC.auxPlus(c, componentShape.inputs)
 
-      override def outputs: Comp0#OutputShape = componentShape.outputs
+      override val outputs: Comp0#OutputShape = componentShape.outputs
     }
+
+  trait Signal {
+    type Contact
+  }
+  type SignalProcessor = Signal => Iterable[Signal]
+
+  trait SignalOnContacts[Contacts <: TypeSet]
+
+  sealed trait Component {
+    type Shape <: ComponentShape
+    type Handler = SignalOnContacts[Shape#InputShape] => Iterable[SignalOnContacts[Shape#OutputShape]]
+    val shape: Shape
+    val handler: Handler
+  }
+
+  type ShapedComponent[S] = Component { type Shape = S}
+
+  def createComponent[CompShape<: ComponentShape,
+    InputContacts <: TypeSet,
+    InputSignal<: SignalOnContacts[InputContacts],
+    OutputSignal <: Signal
+  ](f: InputSignal => Iterable[OutputSignal])(
+    implicit
+    evInputContacts: EachElementIsSubtype[Contact, InputContacts],
+    evInputs: InputContacts ⊂ CompShape#InputShape,
+    evOutputs: OutputSignal#Contact ∊ CompShape#OutputShape
+  ): Component = ???
+
+  def lift[A,B, In <: Contact{ type T = A}, Out <: Contact{ type T = B}](in: In, out: Out, f: A => B):
+    SignalOnContacts[In +: ∅] => Iterable[SignalOnContacts[Out +: ∅]] = ???
+
+  type Plus[Shape1 <: ComponentShape, Shape2 <: ComponentShape] = ComponentShape{
+    type InputShape = Shape1#InputShape ∪ Shape2#InputShape
+    type OutputShape = Shape1#OutputShape ∪ Shape2#OutputShape
+  }
+
+  def addComponent[Shape1 <: ComponentShape, Shape2 <: ComponentShape,
+    Comp1 <: Component{ type Shape = Shape1 },
+    Comp2 <: Component{ type Shape = Shape2 }
+  ](comp1: Comp1, comp2: Comp2)(
+   implicit
+   ev1: UnionHelper[Shape1#InputShape, Shape2#InputShape],
+   ev2: UnionHelper[Shape1#OutputShape, Shape2#OutputShape]
+  ): Component { type Shape = Shape1 Plus Shape2 } = new Component {
+    override type Shape = Shape1 Plus Shape2
+    override val shape: Shape1 Plus Shape2 = new ComponentShape {
+      type InputShape = Shape1#InputShape ∪ Shape2#InputShape
+      type OutputShape = Shape1#OutputShape ∪ Shape2#OutputShape
+      override val inputs: Shape1#InputShape ∪ Shape2#InputShape = ev1.out(comp1.shape.inputs, comp2.shape.inputs)
+      override val outputs: Shape1#OutputShape ∪ Shape2#OutputShape = ev2.out(comp1.shape.outputs, comp2.shape.outputs)
+    }
+    override val handler: Handler = ???
+  }
 }
--- contacts-grid-core/src/test/scala/ru/primetalk/contacts/core/ComponentShapeSpec.scala
+++ contacts-grid-core/src/test/scala/ru/primetalk/contacts/core/ComponentShapeSpec.scala
@@ -8,15 +8,21 @@ class ComponentShapeSpec extends Specification with ComponentShapeBuilderAPI { d
 
     - inputs of the component should be known set $inputsEq
   """
-  abstract class ContactImpl[T](val name: String) extends Product with Serializable
-  override type Contact[T] = ContactImpl[T]
+  sealed trait MyContact {
+    type T
+  }
+  abstract class ContactImpl[A](val name: String) extends Product with Serializable with MyContact {
+    override type T = A
+  }
+  override type Contact = MyContact
   case object In1 extends ContactImpl[String]("In1")
   case object Out1 extends ContactImpl[String]("Out1")
   val myComponent: ComponentShape {
     type InputShape = ConsTypeSet[In1.type, ∅]
     type OutputShape = ∅
-  } = addInput[String, In1.type, EmptyComponentShape.type](In1, EmptyComponentShape)
+  } = addInput[In1.type, EmptyComponentShape.type](In1, EmptyComponentShape)
 
-  val inputs: ConsTypeSet[In1.type, Empty.type] = addElement(In1, ∅)
-  def inputsEq = inputs must be(myComponent.inputs)
+  val inputs: ConsTypeSet[In1.type, ∅] = addElement(In1, ∅)
+  def inputsEq =
+    inputs must be(myComponent.inputs)
 }
