68a8ead41b506463356f358c91c131a8269e397b
Sergey <skazancev@gmail.com>
2019-10-13 20:33:29 +0300

    type set refactoring

--- contacts-grid-core/src/test/scala/ru/primetalk/contacts/core/ComponentShapeSpec.scala
+++ contacts-grid-core/src/test/scala/ru/primetalk/contacts/core/ComponentShapeSpec.scala
@@ -1,8 +1,8 @@
 package ru.primetalk.contacts.core
 
 import org.specs2.Specification
-
 import TypeSets._
+import org.specs2.matcher.MatchResult
 
 class ComponentShapeSpec extends Specification with ComponentShapeBuilderAPI { def is = s2"""
 
@@ -10,21 +10,24 @@ class ComponentShapeSpec extends Specification with ComponentShapeBuilderAPI { d
 
     - inputs of the component should be known set $inputsEq
   """
+
   sealed trait MyContact extends Contact {
     type Data
   }
+
   abstract class ContactImpl[A](val name: String) extends Product with Serializable with MyContact {
     override type Data = A
   }
- // override type Contact = MyContact
+
   case object In1 extends ContactImpl[String]("In1")
   case object Out1 extends ContactImpl[String]("Out1")
+
   val myComponent: ComponentShape {
     type InputShape = ConsTypeSet[In1.type, ∅]
     type OutputShape = ∅
   } = addInput[In1.type, EmptyComponentShape.type](In1, EmptyComponentShape)
 
   val inputs: ConsTypeSet[In1.type, ∅] = addElement(In1, ∅)
-  def inputsEq =
-    inputs === myComponent.inputs
+
+  def inputsEq: MatchResult[In1.type +: ∅] = inputs === myComponent.inputs
 }
