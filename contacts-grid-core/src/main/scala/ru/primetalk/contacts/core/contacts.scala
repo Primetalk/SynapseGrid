c601d7995a9168322fccb5823341a78e9ef14a44
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2019-12-11 21:09:59 +0300

    Add NamedContacts

---
+++ contacts-grid-core/src/main/scala/ru/primetalk/contacts/core/contacts.scala
@@ -0,0 +1,39 @@
+package ru.primetalk.contacts.core
+
+trait Contacts {
+  // customizable contact. User might create specific contacts with different data inside.
+  // For instance, for remoting it might be convenient to have contacts identified by integers.
+  trait Contact {
+    type Data
+  }
+}
+
+trait NamedContacts extends Contacts {
+
+  final class NamedContact[A <: String,B](val name: A) extends Product with Serializable with Contact {
+    override type Data = B
+    override def productArity: Int = 0
+    override def productElement(n: Int): Any = 0
+    override def canEqual(that: Any): Boolean = that.isInstanceOf[NamedContact[_, _]]
+    override def equals(o: Any): Boolean = o match {
+      case n: NamedContact[_,_] => n.name == name
+      case _ => false
+    }
+
+    override def hashCode(): Int = name.hashCode()
+  }
+
+  object NamedContact {
+
+    def apply[A <: String : ValueOf, B]: NamedContact[A,B] =
+      new NamedContact[A, B](implicitly[ValueOf[A]].value)
+
+    implicit def valueOfNamedContact[A <: String : ValueOf, B]: ValueOf[NamedContact[A, B]] =
+      new ValueOf[NamedContact[A, B]](apply[A, B])
+  }
+
+}
+
+trait AutoContacts extends Contacts {
+
+}
